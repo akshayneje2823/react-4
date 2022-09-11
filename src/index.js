4b6e5bc2ec5116bfb5c4654f28ea55408c802581
Akshay Neje <akshayneje2823@gmail.com>
2022-09-11 22:45:30 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './To-Do-List/App';
+import App from './Form Validation/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
